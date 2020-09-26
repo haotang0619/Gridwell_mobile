03ad3d3f4f51177027a192b80bf21906114b308e
HowardTang <b06902020@ntu.edu.tw>
2020-09-26 21:06:19 +0800

    þAdd control page

---
+++ control.php
@@ -0,0 +1,22 @@
+<!-- This is control page, the content is in ./php_control_page directory -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<?php
+/* 登入檢查 */
+// include "all/all_check.php";
+?>
+
+<!-- Specify the character encoding to UTF-8  -->
+<meta charset="UTF-8" />
+<!-- include header -->
+<?php include "./php_control_page/header.php"; ?>
+
+<body>
+  <!-- include body content -->
+  <?php include "./php_control_page/body_content.php"; ?>
+  <!-- include footer -->
+  <?php // include "./php_index_page/footer.php"; 
+  ?>
+</body>
+
+</html>
---
+++ php_control_page/body_content.php
@@ -0,0 +1,52 @@
+<div>
+    <!-- Nav bar start-->
+    <header class="nav_group">
+        <div class="nav_inside">
+            <h6 class="nav_title">自動開關控制頁面</h6>
+        </div>
+    </header>
+    <!-- Nav bar end-->
+
+    <div class="container">
+        <div class="control_header">
+            <button class="button_group control_button" onclick="switchTab(0)" type="button">
+                開 / 關
+            </button>
+            <button class="button_group control_button" onclick="switchTab(1)" type="button">
+                歷史資訊
+            </button>
+            <button class="button_group control_button" onclick="logOut()" type="button">
+                登出
+            </button>
+        </div>
+
+        <div class="select_group">
+            <select class="select_text" onchange="switchImage()" required>
+                <option value=""></option>
+                <option value="1" selected>1</option>
+            </select>
+            <label class="select_label">選擇場域</label>
+        </div>
+
+        <div class="table_group">
+            <table class="table_root" aria-label="table">
+                <thead class="table_head">
+                    <tr class="table_head_tr">
+                        <th class="table_head_th" scope="col">設備名稱</th>
+                        <th class="table_head_th" scope="col">設備內容</th>
+                        <th class="table_head_th" scope="col">設備狀態</th>
+                    </tr>
+                </thead>
+                <tbody class="table_body">
+                    <tr class="table_body_tr">
+                        <td class="table_body_td">1</td>
+                        <td class="table_body_td">ON / OFF</td>
+                        <td class="table_body_td">上線</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <p>請連線至WiFi: powerAP / 1234567890</p>
+    </div>
+</div>
