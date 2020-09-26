4c7e6940cd726d0234e0e3869b5c16a1c25deef0
HowardTang <b06902020@ntu.edu.tw>
2020-09-26 21:06:19 +0800

    Add control page

---
+++ php_control_page/header.php
@@ -0,0 +1,11 @@
+<title>自動開關控制頁面</title>
+
+<!-- General css  -->
+<link href="css/button.css" rel="stylesheet" type="text/css">
+<link href="css/container.css" rel="stylesheet" type="text/css">
+<link href="css/nav.css" rel="stylesheet" type="text/css">
+<link href="css/select.css" rel="stylesheet" type="text/css">
+<link href="css/table.css" rel="stylesheet" type="text/css">
+
+<!-- Page css  -->
+<link href="php_control_page/control.css" rel="stylesheet" type="text/css">
